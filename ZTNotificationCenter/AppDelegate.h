f606c52535831524c20cd8e42cac04f1fb339e57
2464556998@qq.com <2464556998@qq.com>
2017-09-26 14:17:43 +0800

    Initial Commit

---
+++ ZTNotificationCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZTNotificationCenter
+//
+//  Created by 张涛 on 2017/9/26.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
